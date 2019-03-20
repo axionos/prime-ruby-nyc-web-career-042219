169e2daeedda153c7a383c970bbfe833ff194959
SOYEONG OH <axionoso@gmail.com>
2019-03-20 15:28:54 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,10 +6,10 @@
 #     false
 #   end
 # end
-def prime?(number)
+def prime?(number) #11
   start = 2
   if number > 1
-    range = (start..number-1).to_a
+    range = (start..number-1).to_a #[2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     range.none? do |num_to_test|
       number % num_to_test == 0
     end
