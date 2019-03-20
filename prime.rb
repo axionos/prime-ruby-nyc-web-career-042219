c3f13109f1a11031f98886fb973ac708ecd94e0e
SOYEONG OH <axionoso@gmail.com>
2019-03-20 15:07:28 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,15 +6,27 @@
 #     false
 #   end
 # end
-def prime?(number)
-  start = 2
-  if number > 1
-    range = (start..number-1).to_a
-    range.none? do |num_to_test|
-      number % num_to_test == 0
+# def prime?(number)
+#   start = 2
+#   if number > 1
+#     range = (start..number-1).to_a
+#     range.none? do |num_to_test|
+#       number % num_to_test == 0
+#     end
+#   else
+#     false
+#   end
+# end
+
+class Integer
+  def is_prime?
+    prime = true
+    for r in 2..Math.sqrt(self).to_i
+      if (self % r == 0)
+        prime = false
+        break
+      end
     end
-  else
-    false
+    return prime
   end
 end
-prime?(6)
